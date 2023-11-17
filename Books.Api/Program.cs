e1f7cc47decae09ab6d8f90e7b86eef7b19a17ae
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-11-16 18:32:01 -0800

    Added SpecFlow project.

--- Books.Api/Program.cs
+++ Books.Api/Program.cs
@@ -1,15 +1,19 @@
+// <copyright file="Program.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
 namespace Books.Api
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+			app.MapGet("/", () => "Hello World!");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
---
+++ BooksApi.Specs/ImplicitUsings.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="ImplicitUsings.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+global using FluentAssertions;
+global using TechTalk.SpecFlow;
+global using Xunit;

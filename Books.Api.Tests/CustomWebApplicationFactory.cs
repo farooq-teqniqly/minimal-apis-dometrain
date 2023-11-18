0421cb563cdc564b8a7b328a35be8857b7581688
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-11-18 13:20:04 -0800

    Setup projects.

---
+++ Books.Api.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="CustomWebApplicationFactory.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Books.Api.Tests
+{
+	using Microsoft.AspNetCore.Mvc.Testing;
+
+	internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
+	{
+	}
+}
---
+++ Books.Api.Tests/Usings.cs
@@ -0,0 +1,5 @@
+// <copyright file="Usings.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+global using Xunit;
